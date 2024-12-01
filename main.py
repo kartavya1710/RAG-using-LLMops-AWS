1446e3cef5f5c74a7e4273f02e8250169c4e3f9e
kartavya1710 <kartavyamaster17@gmail.com>
2024-12-01 14:22:52 +0530

    code edded for llama2

---
+++ main.py
@@ -0,0 +1,30 @@
+import os
+import json
+from pyexpat import model
+import sys
+from urllib import response
+import boto3
+
+prompt = "You are smart assistant. Please let me know what is machine learning in smartest way"
+
+bedrock = boto3.client(service_name="bedrock-runtime")
+
+payload= {
+
+
+
+}
+
+body = json.dumps(payload)
+model_id = "meta.llama2-70b-chat-v1"
+
+response = bedrock.invoke_model(
+    body=body,
+    modelId=model_id,
+    accept="application/json",
+    contentType="application/json"
+)
+
+response_body = json.loads(response.get("body").read())
+response_text = response_body["generation"]
+print(response_text)
